33a0a9bfb2162c7903d4cb9568bcecb65dcdea48
Gustavo Miranda <oachgustavo@gmail.com>
2019-02-10 14:48:26 -0200

    Ajustes no error-handler.

--- src/app/services/util/error-handler.service.ts
+++ src/app/services/util/error-handler.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { environment } from '../../../environments/environment';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,28 @@ export class ErrorHandlerService {
   constructor(private messageService: MessageService) { }
 
   error(err: any) {
-    let msg = 'Algum erro aconteceu!';
-
+    let msg: string;
     if(typeof err === 'string') {
       msg = err;
     } else {
-      console.log(err);
-      msg = err.error[0].message;
+      msg = this.errorFilter(err);
     }
+    this.showErrorToast(msg);
+  }
 
+  /* Filtra os erros para saber o seu tipo e a mensagem que deve retornar. */
+  private errorFilter(err: any) {
+    let msg: string;
+    if (err instanceof HttpErrorResponse) {
+      if (err.status === 0) {
+        msg = 'Estamos com problemas para se comunicar com o servidor.';
+      }
+    }
+    return msg;
+  }
+
+  /* Mostra a mensagem de erro tratada. */
+  private showErrorToast(msg: string) {
     this.messageService.add({
       severity: 'error',
       summary: environment.ErrorSummaryMessages[
